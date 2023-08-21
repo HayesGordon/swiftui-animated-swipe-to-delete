204efdefa07636e021c38cf6585ef7e65a0ab04f
Gordon Hayes <pggordonhayes@gmail.com>
2023-08-21 15:56:38 +0200

    Initial Commit

---
+++ Alligator Todo/Alligator_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alligator_TodoApp.swift
+//  Alligator Todo
+//
+//  Created by Peter G Hayes on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Alligator_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
